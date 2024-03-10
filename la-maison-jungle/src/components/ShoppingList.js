bd7ff975e9c6d3305c753ccd7b62e632d6870d2f
titouan-aclr <122918576+titouan-aclr@users.noreply.github.com>
2024-03-10 16:44:42 +0100

    P2C4

--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -1,25 +1,34 @@
 import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem.js'
 import '../styles/ShoppingList.css'
 
 function ShoppingList() {
-    const categories = plantList.reduce((accu, plant) => accu.includes(plant.category) ? accu : [...accu, plant.category], [])
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
 	return (
-        <div>
-            <ul>
-                {categories.map((cat) => (
-                    <li key={{cat}}>{cat}</li>
-                ))}
-            </ul>
-            <ul className='lmj-plant-list'>
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-						{plant.name}
-                        {plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
-					</li>
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-        </div>
-    )
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default ShoppingList
